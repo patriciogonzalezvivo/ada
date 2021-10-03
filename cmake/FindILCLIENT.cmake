5a3f4de3c95034f3e5f05e146b8c59ac580ea5ba
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2021-10-03 07:11:49 -0400

    fixing cmake deps

--- cmake/FindILCLIENT.cmake
+++ cmake/FindILCLIENT.cmake
@@ -3,17 +3,17 @@
 #  ILCLIENT_LIBRARIES, the libraries to link against to use PNG.
 #  ILCLEINT_FOUND, If false, do not try to use PNG.
 
-set(ILCLIENT_INCLUDE_DIR /opt/vc/src/hello_pi/libs/ilclient)
+set(ILCLIENT_INCLUDE_DIR 
+    /opt/vc/src/hello_pi/libs/ilclient)
 
 find_library(   ILCLIENT_LIBRARY
                 NAME libilclient.a
                 DOC "Path ILCLIENT Library"
-                PATHS ILCLIENT_INCLUDE_DIR )
-
-mark_as_advanced(ILCLIENT_INCLUDE_DIR ILCLIENT_LIBRARY)
+                PATHS /opt/vc/src/hello_pi/libs/ilclient )
+mark_as_advanced(ILCLIENT_LIBRARY ILCLIENT_INCLUDE_DIR)
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(ilclient 
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Broadcom 
     ILCLIENT_LIBRARY
 )
